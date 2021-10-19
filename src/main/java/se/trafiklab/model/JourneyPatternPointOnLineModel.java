d3b8376a7784e612ff10cf8819d4c916e7eee920
Erik <eriksw54@gmail.com>
2021-10-19 21:47:51 +0200

    Removed unused imports

--- src/main/java/se/trafiklab/model/JourneyPatternPointOnLineModel.java
+++ src/main/java/se/trafiklab/model/JourneyPatternPointOnLineModel.java
@@ -2,10 +2,8 @@ package se.trafiklab.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 public class JourneyPatternPointOnLineModel {
