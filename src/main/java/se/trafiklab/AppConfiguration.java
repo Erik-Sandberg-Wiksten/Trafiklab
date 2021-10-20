cea9ed33d4caffea46328ceb075e9f308cf81255
Erik <eriksw54@gmail.com>
2021-10-20 23:00:23 +0200

    Wired beans

--- src/main/java/se/trafiklab/AppConfiguration.java
+++ src/main/java/se/trafiklab/AppConfiguration.java
@@ -11,6 +11,7 @@ import se.trafiklab.bus.BusLineInfo;
 import se.trafiklab.bus.BusService;
 import se.trafiklab.bus.BusServiceImpl;
 import se.trafiklab.bus.function.GroupByLineNumberFunction;
+import se.trafiklab.bus.function.LineNumberToLineNameFunction;
 import se.trafiklab.bus.function.TopTenBusLineFunction;
 import se.trafiklab.http.Client;
 import se.trafiklab.http.RequestFactory;
@@ -18,12 +19,14 @@ import se.trafiklab.http.impl.ClientImpl;
 import se.trafiklab.http.impl.RequestFactoryImpl;
 import se.trafiklab.model.JourneyPatternPointOnLineModel;
 import se.trafiklab.model.Model;
+import se.trafiklab.model.StopPointModel;
 import se.trafiklab.model.factory.ModelFactory;
 import se.trafiklab.model.factory.impl.ModelFactoryImpl;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 @Configuration
@@ -35,7 +38,12 @@ public class AppConfiguration {
 
     @Bean
     public BusService busService() {
-        return new BusServiceImpl(requestFactory(), client(), modelFactory(), topTenFunction());
+        return new BusServiceImpl(requestFactory(),
+                client(),
+                modelFactory(),
+                topTenFunction(),
+                groupByLineNumberFunction(),
+                lineNumberToLineNameFunction());
     }
 
     @Bean
@@ -67,11 +75,17 @@ public class AppConfiguration {
 
     @Bean
     public Function<Model<JourneyPatternPointOnLineModel>, List<BusLineInfo>> topTenFunction() {
-        return new TopTenBusLineFunction(groupByLineNumber());
+        return new TopTenBusLineFunction(groupByLineNumberFunction());
     }
 
     @Bean
-    public Function<Model<JourneyPatternPointOnLineModel>, Map<Integer, Set<Integer>>> groupByLineNumber() {
+    public Function<Model<JourneyPatternPointOnLineModel>, Map<Integer, Set<Integer>>> groupByLineNumberFunction() {
         return new GroupByLineNumberFunction();
     }
+
+    @Bean
+    public BiFunction<Model<StopPointModel>, Set<Integer>, Set<String>> lineNumberToLineNameFunction() {
+        return new LineNumberToLineNameFunction();
+    }
+
 }
