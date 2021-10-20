dcdef45618fce02900951444652acdf41e7261a6
Erik <eriksw54@gmail.com>
2021-10-20 04:58:07 +0200

    Added global controller exception handler

---
+++ src/main/java/se/trafiklab/GlobalControllerExceptionHandler.java
@@ -0,0 +1,15 @@
+package se.trafiklab;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(Exception.class)
+    public void handleConflict() {
+
+    }
+}
