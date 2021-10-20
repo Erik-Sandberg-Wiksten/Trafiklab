c0336bc5f49cdea9cf7e07d8d9e81562eb7d5fbf
Erik <eriksw54@gmail.com>
2021-10-20 22:59:06 +0200

    Added global exception handler

--- src/main/java/se/trafiklab/GlobalControllerExceptionHandler.java
+++ src/main/java/se/trafiklab/GlobalControllerExceptionHandler.java
@@ -1,15 +1,16 @@
 package se.trafiklab;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
-    @ResponseStatus(HttpStatus.CONFLICT)
-    @ExceptionHandler(Exception.class)
-    public void handleConflict() {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleConflict(Exception e) {
+        return ResponseEntity.badRequest().body("Invalid line number");
     }
 }
