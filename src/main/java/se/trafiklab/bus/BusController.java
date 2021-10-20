9364899dc2d172713a318770a1d5985e5de0b48b
Erik <eriksw54@gmail.com>
2021-10-20 22:58:50 +0200

    Added new endpoint to controller

--- src/main/java/se/trafiklab/bus/BusController.java
+++ src/main/java/se/trafiklab/bus/BusController.java
@@ -1,9 +1,11 @@
 package se.trafiklab.bus;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BusController {
@@ -16,6 +18,15 @@ public class BusController {
     @GetMapping("/busline")
     List<BusLineInfo> getBusLineInfo(){
         return service.getTopTenBusLines();
+    }
+
+    @GetMapping("/busline/{lineNumber}")
+    BusLineRouteInfo getBusLineInfo(@PathVariable(name="lineNumber") Integer lineNumber) throws Exception {
+        Optional<BusLineRouteInfo> busLineRouteInfo = service.getRouteInfoByLineNumber(lineNumber);
+        if(busLineRouteInfo.isEmpty()) {
+            throw new Exception("Line number " + lineNumber + " was not found");
+        }
+        return busLineRouteInfo.get();
     }
 
 }
